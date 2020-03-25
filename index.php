601594fd94987a2fdb0d6d3fbc5d9784ed367982
tori <torielisehart@gmail.com>
2020-03-25 11:10:06 -0400

    Initializing new site, removed php.

--- index.php
+++
@@ -1,7 +0,0 @@
-<?php include '/bless-the-connection.php'; ?>
-<?php include '/header-home.php'; ?>
-    <main>
-    <?php include '/offerings.php'; ?>
-    <?php include '/contact.php'; ?>
-    </main>
-<?php include '/footer.php'; ?>
